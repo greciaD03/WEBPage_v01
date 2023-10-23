e18d5f9365036bfa2e3b9d30da3ac0772cd84bc5
greciaD03 <grecia.deyanira.mtz2003@gmail.com>
2023-10-23 11:04:10 -0600

    Conexion via POST

---
+++ src/Controller/PostController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Post; 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PostController extends AbstractController
+{
+    #[Route('/post/{id}', name: 'app_post')]
+    public function index(Post $post): Response
+    {
+        dump($post); 
+        return $this->render('post/index.html.twig', [
+            'controller_name' => [
+                'nombre' => 'Grecia', 
+                'apellido' => 'Martinez',
+            ],
+            'post' => $post 
+        ]); 
+    }
+}
